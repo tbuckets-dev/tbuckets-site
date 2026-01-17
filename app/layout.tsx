04fe6961003b8f2206840c01c758ff3eb6405b4c
tbuckets-dev <twitt23@gmail.com>
2026-01-17 14:52:34 +0000

    Changed header

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Vessel | T.Buckets",
+  title: "TBuckets",
   description: "IT Infrastructure & Modern DevOps Portfolio",
 };
 
